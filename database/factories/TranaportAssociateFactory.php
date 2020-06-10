0e3a2db56f083c690c51e4c49f62035b97b51458
Farhan Tanvir <farhan15-7471@diu.edu.bd>
2020-06-11 02:42:48 +0600

    transport associates

---
+++ database/factories/TranaportAssociateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\TranaportAssociate;
+use Faker\Generator as Faker;
+
+$factory->define(TranaportAssociate::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
